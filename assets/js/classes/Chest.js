71f75fa9099da1e40c40ae749754a2ec43461196
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-05-02 15:00:54 -0700

    chests spawn randomly and chest coins random

--- assets/js/classes/Chest.js
+++ assets/js/classes/Chest.js
@@ -2,10 +2,16 @@ class Chest extends Phaser.Physics.Arcade.Image {
   constructor(scene, x, y, key, frame) {
     super(scene, x, y, key, frame);
     this.scene = scene;
-    this.coins = 10;
+    this.coins = this.getRandomInt(2, 20);
 
     this.scene.physics.world.enable(this);
 
     this.scene.add.existing(this);
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 }
--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -5,40 +5,81 @@ class GameScene extends Phaser.Scene {
 
   init() {
     this.scene.launch("Ui");
+    this.score = 0;
   }
 
   create() {
-    this.goldPickupAudio = this.sound.add("goldSound", {
-      loop: false,
-      volume: 0.2,
-    });
-
-    this.chest = new Chest(this, 300, 300, "items", 0);
-
-    this.wall = this.physics.add.image(500, 100, "button1");
-    this.wall.setImmovable();
-
-    this.player = new Player(this, 32, 32, "characters", 0);
-
-    this.physics.add.collider(this.player, this.wall);
-    this.physics.add.overlap(
-      this.player,
-      this.chest,
-      this.collectChest,
-      null,
-      this
-    );
-
-    this.cursors = this.input.keyboard.createCursorKeys();
+    this.createAudio();
+    this.createChests();
+    this.createWalls();
+    this.createPlayer();
+    this.addCollisions();
+    this.createInput();
   }
 
   update() {
     this.player.update(this.cursors);
   }
 
+  createAudio() {
+    this.goldPickupAudio = this.sound.add("goldSound", {
+      loop: false,
+      volume: 0.2,
+    });
+  }
+
+  createPlayer() {
+    this.player = new Player(this, 32, 32, "characters", 0);
+  }
+
+  createChests() {
+    this.chests = this.physics.add.group();
+    this.chestPositions = [
+      [100, 100],
+      [200, 200],
+      [300, 300],
+      [400, 400],
+      [500, 500],
+    ];
+    this.maxNumberOfChests = 3;
+    for (let i = 0; i < this.maxNumberOfChests; i += 1) {
+      this.spawnChest();
+    }
+  }
+
+  spawnChest() {
+    const location = this.chestPositions[
+      Math.floor(Math.random() * this.chestPositions.length)
+    ];
+    const chest = new Chest(this, location[0], location[1], "items", 0);
+    this.chests.add(chest);
+  }
+
+  createWalls() {
+    this.wall = this.physics.add.image(500, 100, "button1");
+    this.wall.setImmovable();
+  }
+
+  createInput() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  addCollisions() {
+    this.physics.add.collider(this.player, this.wall);
+    this.physics.add.overlap(
+      this.player,
+      this.chests,
+      this.collectChest,
+      null,
+      this
+    );
+  }
+
   collectChest(player, chest) {
     this.goldPickupAudio.play();
-    this.events.emit("updateScore", chest.coins);
+    this.score += chest.coins;
+    this.events.emit("updateScore", this.score);
     chest.destroy();
+    this.time.delayedCall(1000, this.spawnChest, [], this);
   }
 }
