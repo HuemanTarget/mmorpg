e3de9527272972b3e422e30a36fe7dd060376f6f
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-05-02 09:40:39 -0700

    create canvas add js and html file

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+  },
+};
+
+let game = new Phaser.Game(config);
+
+function preload() {}
+
+function create() {}
