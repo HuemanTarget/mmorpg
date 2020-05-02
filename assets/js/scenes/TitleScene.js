15500ceceb44ff2d6688829359c98274905423a7
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-05-02 13:31:19 -0700

    add title scene and functionality

--- assets/js/scenes/TitleScene.js
+++ assets/js/scenes/TitleScene.js
@@ -3,11 +3,38 @@ class TitleScene extends Phaser.Scene {
     super("Title");
   }
 
-  preload() {
-
-  }
-
   create() {
-    
+    this.titleText = this.add.text(
+      this.scale.width / 2,
+      this.scale.height / 2,
+      "Zenva MMORPG",
+      { fontSize: "64px", fill: "white" }
+    );
+    this.titleText.setOrigin(0.5);
+    this.button = this.add.image(
+      this.scale.width / 2,
+      this.scale.height * 0.65,
+      "button1"
+    );
+
+    this.button.setInteractive();
+
+    this.buttonText = this.add.text(0, 0, "Start", {
+      fontSize: "26px",
+      fill: "white",
+    });
+    Phaser.Display.Align.In.Center(this.buttonText, this.button);
+
+    this.button.on("pointerdown", () => {
+      this.scene.start("Game");
+    });
+
+    this.button.on("pointerover", () => {
+      this.button.setTexture("button2");
+    });
+
+    this.button.on("pointerout", () => {
+      this.button.setTexture("button1");
+    });
   }
 }
